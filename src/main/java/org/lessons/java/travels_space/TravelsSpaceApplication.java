63cf44364316a959584d323f60b59f8934b08662
Andre050101 <agiancristiano90@gmail.com>
2025-05-13 17:44:29 +0200

    first commit

---
+++ src/main/java/org/lessons/java/travels_space/TravelsSpaceApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.java.travels_space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelsSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelsSpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lessons/java/travels_space/TravelsSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.java.travels_space;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelsSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
